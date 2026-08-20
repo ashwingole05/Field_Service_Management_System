0708ba68b502400fae387b710ac668b34162d496
Ashwin Gole <ashwingole05@gmail.com>
2026-08-20 22:43:49 +0530

    Restore last working backend deployment configuration

--- src/main/java/com/FieldService/Security/SecurityConfig.java
+++ src/main/java/com/FieldService/Security/SecurityConfig.java
@@ -2,20 +2,15 @@ package com.FieldService.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
@@ -48,9 +43,6 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
-
-                // Enable CORS using CorsConfig.java
-                .cors(cors -> {})
 
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(
